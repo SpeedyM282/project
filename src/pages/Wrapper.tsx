d456272a8433135520a26204847211484d7edaf4
speedym282 <abdulaxadrizakulov@gmail.com>
2024-04-22 14:06:23 +0500

    resolve bug

--- src/pages/Wrapper.tsx
+++ src/pages/Wrapper.tsx
@@ -1,6 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Wrapper = () => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			navigate("/dashboard");
+		} else {
+			navigate("/login");
+		}
+	}, []);
+
 	return (
 		<>
 			<Outlet />
